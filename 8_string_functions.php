22549388970da6f86d8cef7a79b401af440235fe
John Paul Lazaro <john-paul.lazaro@atos.net>
2022-12-08 17:40:19 +0800

    added array info

---
+++ 8_string_functions.php
@@ -0,0 +1,14 @@
+<?php
+     
+    
+    // strlen($string)
+    // strrev($string)
+    // strtolower($string)
+    // strtoUpper($string)
+    // str_replace('want','replacement',$string)
+    // substr($string, 0, 5);
+    // str_ends_with($string,'a');
+    // str_ends_with($string,'a');
+
+    //htmlspecialchars($str);
+?>
---
+++ 9_arrays_functions.php
@@ -0,0 +1,29 @@
+<?php 
+
+    // ARRAY FUNCTIONS
+    
+    //1.Count 
+    //count($arrayname)
+    
+    //2.Search
+    //in_array('searchvalue',$arrayname);
+    
+    //3.ADD to end
+    //array_push($arrayname,'newitem');
+    
+    //4.ADD to start
+    //array_unshift($arrayname,'newitem');
+    
+    //5.Delete item from beginning
+    //array_pop($arrayname);
+    
+    //6.Delete item from end
+    //array_shift($arrayname);
+    
+    //7. Delete specific
+    //unset($arrayname[0]);
+
+
+    //8. Array Combine
+    array_merge()
+?>
